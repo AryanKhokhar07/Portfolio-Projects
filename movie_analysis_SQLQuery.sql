8fdb7c10c84c46435c0a7da21835c2cf82fb25a6
AryanKhokhar07 <97471424+AryanKhokhar07@users.noreply.github.com>
2023-06-28 21:54:59 +0530

    Add files via upload

---
+++ movie_analysis_SQLQuery.sql
@@ -0,0 +1,86 @@
+/*           CLEANING DATA IN SQL QUERIES            */
+
+
+
+
+SELECT *
+FROM movies.dbo.movie
+
+----------------------------------------------------------------------------------------------
+
+-- Looking for any duplicate rows
+
+
+SELECT COUNT(name) AS total_obs
+FROM movies.dbo.movie
+
+SELECT DISTINCT COUNT(name) AS total_distinct_obs
+FROM movies.dbo.movie
+
+----------------------------------------------------------------------------------------------
+
+-- Replacing null values in rating column
+
+
+UPDATE movies.dbo.movie
+SET rating = 'Not Rated'
+WHERE rating is NULL
+
+----------------------------------------------------------------------------------------------
+
+-- Calculating avg budget and gross for each genre
+
+
+SELECT 
+	genre,
+	AVG(budget) AS avg_budget,
+	AVG(gross) AS avg_revenue
+FROM movies.dbo.movie
+GROUP BY genre
+ORDER BY avg_revenue DESC
+
+-----------------------------------------------------------------------------------------------
+
+-- Top most rated directors
+
+
+SELECT TOP 10
+	director,
+	AVG(score) AS avg_score
+FROM movies.dbo.movie
+GROUP BY director
+ORDER BY avg_score DESC
+
+-----------------------------------------------------------------------------------------------
+
+-- Top 250 best rated movies
+
+
+SELECT TOP 250
+	name, score, director, writer, star, country, company, budget, gross
+FROM movies.dbo.movie
+ORDER BY score DESC
+
+-----------------------------------------------------------------------------------------------
+
+-- Most profitable movies
+
+
+SELECT name, score,director, star, country,
+	(gross-budget) AS profit
+FROM movies.dbo.movie
+ORDER BY profit DESC
+
+-----------------------------------------------------------------------------------------------
+
+-- Most watched genre
+
+
+SELECT
+	genre,
+	COUNT(genre) AS count_of_genres
+FROM movies.dbo.movie
+GROUP BY genre
+ORDER BY count_of_genres DESC
+
+-----------------------------------------------------------------------------------------------
